98fadb963abe7868c3ae1cba58a5331559964be4
Brandon Woodford <brandon.woodford.work@gmail.com>
2025-12-12 19:16:41 -0600

    Finish day two part two

--- days/two.py
+++ days/two.py
@@ -4,21 +4,67 @@ import math
 def is_even(number):
     return number % 2 == 0
 
+def find_factors(number):
+    factors = []
+    limit = int(number**0.5)
+
+    for i in range(1, limit + 1):
+        if number % i == 0:
+            factors.append(i)
+
+            larger_factor = number // i
+
+            if larger_factor != i and larger_factor != number:
+                factors.append(larger_factor)
+
+    factors.sort(reverse=True)
+    return factors
+
 
 def part_one(input: list[tuple]) -> int:
     total = 0
 
     for start, end in input:
-        for val in range(start, end + 1):
-            val = str(val)
-            val_len = len(val)
-            if val_len > 1 and is_even(val_len):
-                half = math.floor(val_len / 2)
-                if val[:half] == val[half:]:
-                    total += int(val)
+        for num in range(start, end + 1):
+            str_num = str(num)
+            num_len = len(str_num)
+            if num_len > 1 and is_even(num_len):
+                half = num_len // 2
+                if str_num[:half] == str_num[half:]:
+                    total += int(str_num)
 
     return total
 
 
+def get_pattern(factor: int, number: str):
+    if factor == 1:
+        return number[0]
+    return number[:factor]
+
 def part_two(input: list[tuple]) -> int:
-    pass
+    total = 0
+
+    for start, end in input:
+        for num in range(start, end + 1):
+            str_num = str(num)
+            num_len = len(str_num)
+            if num_len > 1:
+                factors = find_factors(num_len)
+                i = 0
+
+                while i < len(factors):
+                    matches = True
+                    factor = factors[i]
+                    pattern = get_pattern(factor, str_num)
+                    for j in range(factor, num_len, factor):
+                        if pattern != str_num[j:factor+j]:
+                            matches = False
+                            break
+
+                    if matches:
+                        total += num
+                        break
+
+                    i += 1
+
+    return total
--- tests/two_test.py
+++ tests/two_test.py
@@ -1,3 +1,4 @@
+import pytest
 from days.two import part_one, part_two
 
 
@@ -35,4 +36,106 @@ def test_part_one_example():
 def test_part_two():
     input = []
     with open("data/two.txt", "r") as file:
-        pass
+        line = file.readline()
+        ranges = line.split(",")
+
+        for r in ranges:
+            start_end = r.split("-")
+            input.append((int(start_end[0]), int(start_end[1])))
+
+    assert part_two(input) == 20942028255
+
+def test_part_two_example():
+    input = [
+        (11, 22),
+        (95, 115),
+        (998, 1012),
+        (1188511880, 1188511890),
+        (222220, 222224),
+        (1698522, 1698528),
+        (446443, 446449),
+        (38593856, 38593862),
+        (565653, 565659),
+        (824824821, 824824827),
+        (2121212118, 2121212124),
+    ]
+
+    assert part_two(input) == 4174379265
+
+
+def test_part_two_1():
+    input = [
+        (11, 22),
+    ]
+
+    assert part_two(input) == 33
+
+def test_part_two_2():
+    input = [
+        (95, 115),
+    ]
+    assert part_two(input) == 210
+
+
+def test_part_two_3():
+    input = [
+        (998, 1012),
+    ]
+    assert part_two(input) == 2009
+
+def test_part_two_4():
+    input = [
+        (1188511880, 1188511890),
+    ]
+    assert part_two(input) == 1188511885
+
+
+def test_part_two_5():
+    input = [
+        (222220, 222224),
+    ]
+    assert part_two(input) == 222222
+
+def test_part_two_6():
+    input = [
+        (1698522, 1698528),
+    ]
+    assert part_two(input) == 0
+
+def test_part_two_7():
+    input = [
+        (446443, 446449),
+    ]
+    assert part_two(input) == 446446
+
+def test_part_two_8():
+    input = [
+        (38593856, 38593862),
+    ]
+    assert part_two(input) == 38593859
+
+def test_part_two_9():
+    input = [
+        (565653, 565659),
+    ]
+    assert part_two(input) == 565656
+
+def test_part_two_10():
+    input = [
+        (824824821, 824824827),
+    ]
+    assert part_two(input) == 824824824
+
+def test_part_two_11():
+    input = [
+        (2121212118, 2121212124),
+    ]
+    assert part_two(input) == 2121212121
+
+
+def test_part_two_11():
+    input = [
+        (2121212121, 2121212121),
+    ]
+    assert part_two(input) == 2121212121
+
