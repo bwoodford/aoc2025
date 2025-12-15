f8093fe1094f3e8a45391a2ed24789d26c538ac2
Brandon Woodford <brandon.woodford.work@gmail.com>
2025-12-14 20:24:37 -0600

    Finish day four part one

---
+++ days/four.py
@@ -0,0 +1,45 @@
+
+def part_one(input: list[list[str]]) -> int:
+
+    MAX_ROLLS = 3
+    PAPER = "@"
+    accessed = 0
+    len_rows = len(input)
+    len_cols = len(input[0])
+
+    for i in range(len_rows):
+        for j in range(len_cols):
+
+            if input[i][j] != PAPER:
+                continue
+
+            adj_rolls = 0
+
+            for dr in range(-1, 2):
+                for dc in range(-1, 2):
+
+                    # Skip the current item we're testing from
+                    if dr == 0 and dc == 0:
+                        continue
+
+                    nr = i + dr
+                    nc = j + dc
+
+                    # The 0 equality checks are used to keep us from counting
+                    # the current element.
+                    if not (0 <= nr < len_rows and 0 <= nc < len_cols):
+                        continue
+
+                    if input[nr][nc] == PAPER:
+                        adj_rolls += 1
+
+            if adj_rolls <= MAX_ROLLS:
+                accessed += 1
+
+    return accessed
+
+
+
+
+def part_two(input: list[list[str]]) -> int:
+    pass
---
+++ tests/four_test.py
@@ -0,0 +1,56 @@
+import pytest
+from days.four import part_one, part_two
+
+def test_part_one():
+    input = []
+    with open("data/four.txt", "r") as file:
+        for line in file.readlines():
+            row = []
+            positions = line.strip()
+            for position in positions:
+                row.append(position)
+            input.append(row)
+    assert part_one(input) == 1495
+
+def test_part_one_example():
+    input = [
+        [".", ".", "@", "@", ".", "@", "@", "@", "@", "."],
+        ["@", "@", "@", ".", "@", ".", "@", ".", "@", "@"],
+        ["@", "@", "@", "@", "@", ".", "@", ".", "@", "@"],
+        ["@", ".", "@", "@", "@", "@", ".", ".", "@", "."],
+        ["@", "@", ".", "@", "@", "@", "@", ".", "@", "@"],
+        [".", "@", "@", "@", "@", "@", "@", "@", ".", "@"],
+        [".", "@", ".", "@", ".", "@", ".", "@", "@", "@"],
+        ["@", ".", "@", "@", "@", ".", "@", "@", "@", "@"],
+        [".", "@", "@", "@", "@", "@", "@", "@", "@", "."],
+        ["@", ".", "@", ".", "@", "@", "@", ".", "@", "."]
+    ]
+    assert part_one(input) == 13
+
+@pytest.mark.skip()
+def test_part_two():
+    input = []
+    with open("data/four.txt", "r") as file:
+        for line in file.readlines():
+            row = []
+            positions = line.strip()
+            for position in positions:
+                row.append(position)
+            input.append(row)
+    assert part_two(input) == 0
+
+@pytest.mark.skip()
+def test_part_two_example():
+    input = [
+        [".", ".", "@", "@", ".", "@", "@", "@", "@", "."],
+        ["@", "@", "@", ".", "@", ".", "@", ".", "@", "@"],
+        ["@", "@", "@", "@", "@", ".", "@", ".", "@", "@"],
+        ["@", ".", "@", "@", "@", "@", ".", ".", "@", "."],
+        ["@", "@", ".", "@", "@", "@", "@", ".", "@", "@"],
+        [".", "@", "@", "@", "@", "@", "@", "@", ".", "@"],
+        [".", "@", ".", "@", ".", "@", ".", "@", "@", "@"],
+        ["@", ".", "@", "@", "@", ".", "@", "@", "@", "@"],
+        [".", "@", "@", "@", "@", "@", "@", "@", "@", "."],
+        ["@", ".", "@", ".", "@", "@", "@", ".", "@", "."]
+    ]
+    assert part_two(input) == 0
