3c6f542c42a9de6a7c159f51633bc666779a6914
Brandon Woodford <brandon.woodford.work@gmail.com>
2026-02-14 11:40:49 -0600

    Finish day seven
    Guess I forgot to add part one as its own commit...woops

---
+++ days/seven.py
@@ -0,0 +1,68 @@
+def part_one(input: list[str]) -> int:
+
+    if len(input) == 0:
+        return 0
+
+    line_len = len(input[0])
+    beams = set(i for i, val in enumerate(input[0]) if val == "S")
+    total = 0
+
+    for row in input[1:]:
+        splitters = set(i for i, val in enumerate(row) if val == "^")
+        # Find the beams that are being split
+        intersect = beams.intersection(splitters)
+        # Keep the beams that haven't been split
+        difference = beams.difference(splitters)
+        total += len(intersect)
+
+        if len(intersect) == 0:
+            continue
+
+        new_beams = set()
+        for split in intersect:
+            if split > 0:
+                new_beams.add(split-1)
+            if split < line_len-1:
+                new_beams.add(split+1)
+
+        beams = new_beams.union(difference)
+
+    return total
+
+def part_two(input: list[str]) -> int:
+
+    if len(input) == 0:
+        return 0
+
+    s_index = next(i for i, val in enumerate(input[0]) if val == "S")
+    path_dict = {s_index: 1}
+
+    for i, row in enumerate(input[1:]):
+        splitters = set(i for i, val in enumerate(row) if val == "^")
+
+        if len(splitters) == 0:
+            continue
+
+        buff_dict = {}
+        for (path, timelines) in path_dict.items():
+
+            if path not in splitters:
+                if path not in buff_dict:
+                    buff_dict[path] = timelines
+                else:
+                    buff_dict[path] += timelines
+
+            else:
+                if path-1 not in buff_dict:
+                    buff_dict[path-1] = timelines
+                else:
+                    buff_dict[path-1] += timelines
+
+                if path+1 not in buff_dict:
+                    buff_dict[path+1] = timelines
+                else:
+                    buff_dict[path+1] += timelines
+
+        path_dict = buff_dict
+
+    return sum(path_dict.values())
---
+++ tests/seven_test.py
@@ -0,0 +1,63 @@
+import pytest
+from days.seven import part_one, part_two
+
+file_loc = "data/seven.txt"
+
+def test_part_one():
+    input = []
+    with open(file_loc, "r") as file:
+        for line in file.readlines():
+            input.append(line.strip())
+
+    assert part_one(input) == 1533 
+
+def test_part_one_example():
+    input = [
+        ".......S.......",
+        "...............",
+        ".......^.......",
+        "...............",
+        "......^.^......",
+        "...............",
+        ".....^.^.^.....",
+        "...............",
+        "....^.^...^....",
+        "...............",
+        "...^.^...^.^...",
+        "...............",
+        "..^...^.....^..",
+        "...............",
+        ".^.^.^.^.^...^.",
+        "...............",
+    ]
+
+    assert part_one(input) == 21 
+
+def test_part_two_example():
+    input = [
+        ".......S.......",
+        "...............",
+        ".......^.......",
+        "...............",
+        "......^.^......",
+        "...............",
+        ".....^.^.^.....",
+        "...............",
+        "....^.^...^....",
+        "...............",
+        "...^.^...^.^...",
+        "...............",
+        "..^...^.....^..",
+        "...............",
+        ".^.^.^.^.^...^.",
+        "...............",
+    ]
+    assert part_two(input) == 40 
+
+def test_part_two():
+    input = []
+    with open(file_loc, "r") as file:
+        for line in file.readlines():
+            input.append(line.strip())
+
+    assert part_two(input) == 10733529153890
